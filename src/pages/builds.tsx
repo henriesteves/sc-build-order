31f1c12b33116e66a48f2cba8905c7eceb3ba65a
Henrique Esteves <henriesteves@gmail.com>
2023-01-10 23:06:23 -0300

    clean up some lint issues

--- src/pages/builds.tsx
+++ src/pages/builds.tsx
@@ -1,9 +1,7 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import { useRouter } from "next/router";
 
 import { api } from "../utils/api";
-import { useState } from "react";
 import Link from "next/link";
 
 const BuildsPage: NextPage = () => {
